79a5c136c1348ba581041f161baf7b5aa409bc3e
Xyrox47 <113539295+Xyrox48@users.noreply.github.com>
2024-02-02 10:57:43 -0500

    .

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,7 +1,5 @@
 package COS225_HW1;
 
-import java.lang.reflect.Array;
-
 public class ParkingLot {
     public ParkingSpot[] spots;
     public Car parkingCar;
@@ -16,22 +14,54 @@ public class ParkingLot {
                 this.spots[i] = new ParkingSpot(false);
             }
         }
-
     }
 
     public String toString(){
-        return "";
-    }
+        int handicapOpen = 0;
+        int otherOpen = 0;
+        for (int i = 0; i < 10; i++){
+
+            if (i <= 1){
+                if (spots[i].parkedCar == null){
+                    System.out.println(spots[i].parkedCar);
+                    handicapOpen++;
+                }
+            }
+            else if (i <= 10){
+                if (spots[i].parkedCar == null){
+                    otherOpen++;
+                    System.out.println(spots[i].parkedCar);
+                    // Ask question
+                }
+            }
+            }
+            String str2 = Integer.toString(otherOpen);
+            String str1 = Integer.toString(handicapOpen);
+
+    
+            return str1+" "+ str2;
+        }
+
+
+    
     public int ParkCar(Car parkingCar){
         if (parkingCar.handicap = true){
-            System.out.println("True");
+            if (spots[0].parkedCar == null){
+                spots[0].parkedCar = parkingCar;
+                return 0;
+            }
         }
-        spots[0].parkedCar = parkingCar;
-        System.out.println(spots[0].parkedCar.make);
+        else{
+            spots[2].parkedCar = parkingCar;
+            return 2;
+        }
+        return 0;
+    }
+    public void RemoveCar(int space){
+        spots[space].parkedCar = null;
+    }
 
-        
 
-        return 1;
 
     }
-}
+
--- ParkingTester.java
+++ ParkingTester.java
@@ -4,7 +4,20 @@ public class ParkingTester {
     ParkingLot parkingLot;
     public static void main(String []args){
         ParkingLot parkingLot1 = new ParkingLot();
-        Car Toyoto = new Car("Accord","Toyota", true);
-        parkingLot1.ParkCar(Toyoto);
+
+        System.out.println(parkingLot1.toString());
+        Car InfinitiA = new Car("A","Ifiniti", true);
+        parkingLot1.ParkCar(InfinitiA);
+
+        System.out.println(parkingLot1.toString());
+        Car CadiliacB = new Car("B","Cadiliac", false);
+        parkingLot1.ParkCar(CadiliacB);
+
+        System.out.println(parkingLot1.toString());
+        parkingLot1.RemoveCar(0);
+        
+        System.out.println(parkingLot1.toString());
+
+
     }
 }
